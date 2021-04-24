b2fff27d93e8c3ee65a9e79bdcc8cd7b16f8885b
João Garcia <joao.garcia@iteris.com.br>
2021-04-24 14:47:11 -0300

    feat: dotnet proj

---
+++ src/string/arvore-natal.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Arvore.Natal
+{
+    class ArvoreNatal
+    {
+        public static void main(string[] args)
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
